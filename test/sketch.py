c77f40ca298919b6173542fcbc9e7d093f6b9a50
AryaDwiAditiya <aryadwiaditiya5@gmail.com>
2020-09-04 10:58:32 +0700

    pencil-sketch

--- test/sketch.py
+++ test/sketch.py
@@ -3,13 +3,13 @@ import cv2
 import imageio
 import scipy.ndimage
 
-img = 'img/example4.jpg'
+img = 'img/pic.jpeg'
 
 def greyscale(rgb):
     return np.dot(rgb[...,:3],[0.299,0.587,0.114])
 
 def dodge(front,back):
-    result=front*230/(255-back)
+    result=front*240/(255-back)
     result[result>255]=255
     result[back==255]=255
     return result.astype('uint8')
@@ -24,4 +24,4 @@ b = scipy.ndimage.filters.gaussian_filter(s,sigma=10)
 
 r = dodge(b,g)
 
-cv2.imwrite('img/sketch4.png', r)
+cv2.imwrite('img/sketch5.png', r)
